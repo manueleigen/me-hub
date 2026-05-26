b887992143b9554507ac4d37c55bc555d8b3483c
Manuel Eigen <mail@manueleigen.de>
2026-05-27 00:50:08 +0200

    middleware update

--- middleware.ts
+++
@@ -1,8 +0,0 @@
-import type { NextRequest } from "next/server";
-import { proxy as runProxy } from "./proxy";
-
-export function middleware(request: NextRequest) {
-	return runProxy(request);
-}
-
-export { config } from "./proxy";
